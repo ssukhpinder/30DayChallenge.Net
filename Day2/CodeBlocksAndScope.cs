ccc88e1cd8a6acc31deb73e1aa74841cccdfe99a
Sukhpinder Singh <66723473+sukhsukhpinder@users.noreply.github.com>
2024-03-19 13:36:46 +0530

    Added Day 2 - Variable Scope & Logic Control with Code Blocks

---
+++ Day2/CodeBlocksAndScope.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30DayChallenge.Net.Day2
+{
+    public static class CodeBlocksAndScope
+    {
+        /// <summary>
+        /// Output
+        /// Inside the code block: 10
+        /// </summary>
+        public static void VariableInCodeBlock()
+        {
+            bool flag = true;
+            if (flag)
+            {
+                int value = 10;
+                Console.WriteLine($"Inside the code block: {value}");
+            }
+        }
+
+        /// <summary>
+        /// Outputs
+        /// Program.cs(7,46): error CS0103: The name 'value' does not exist in the current context
+        /// </summary>
+        public static void VariableOutCodeBlock()
+        {
+            bool flag = true;
+            if (flag)
+            {
+                int value = 10;
+                Console.WriteLine($"Inside the code block: {value}");
+            }
+
+            //Uncomment below line to validate
+            //Console.WriteLine($"Outside the code block: {value}");
+        }
+
+        /// <summary>
+        /// Outputs
+        /// Program.cs(6,49): error CS0165: Use of unassigned local variable 'value'
+        /// </summary>
+        public static void VariableAboveCodeBlock()
+        {
+            bool flag = true;
+            int value;
+
+            if (flag)
+            {
+                //Uncomment below line to validate
+                //Console.WriteLine($"Inside the code block: {value}");
+            }
+
+            value = 10;
+            Console.WriteLine($"Outside the code block: {value}");
+        }
+
+        /// <summary>
+        /// Outputs
+        /// Inside the code block: 0
+        /// Outside the code block: 10
+        /// </summary>
+        /// <returns></returns>
+        public static void VariableAboveCodeBlockv1()
+        {
+            bool flag = true;
+            int value = 0;
+
+            if (flag)
+            {
+                Console.WriteLine($"Inside the code block: {value}");
+            }
+
+            value = 10;
+            Console.WriteLine($"Outside the code block: {value}");
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -4,11 +4,21 @@
 #region Day 1 - Expressions
 
 using _30DayChallenge.Net.Day1;
+using _30DayChallenge.Net.Day2;
 
 Expressions.CheckEqualityOperator();
 Expressions.CheckEqualityBuiltInMethods();
 Expressions.CheckInEqualityOperator();
 Expressions.CheckComparisonOperator();
 Expressions.CheckBooleanMethods();
+
+#endregion
+
+#region Day 2 - Variable Scope & Logic Control with Code Blocks
+
+CodeBlocksAndScope.VariableInCodeBlock();
+CodeBlocksAndScope.VariableOutCodeBlock();
+CodeBlocksAndScope.VariableAboveCodeBlock();
+CodeBlocksAndScope.VariableAboveCodeBlockv1();
 
 #endregion
